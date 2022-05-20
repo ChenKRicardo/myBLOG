98d5deebd70c7fdb8054e6c6ba1ba3966d0fb58a
Ricardo陈 <96281398+CKIVZy@users.noreply.github.com>
2022-05-20 20:44:05 +0800

    chore:格式化配置

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/columnList/index.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1',
+    description: '这是第一个测试'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    description: '这是第二个测试',
+    avator: './assets/hutao.gif'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    description: '这是第二个测试'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    description: '这是第三个测试'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: { ColumnList },
+  setup() {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style></style>
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve('./src')
+    }
+  }
+})
